a5059459bd0d25eb0fca828a834e81e5dbc3730d
Eric13531 <zhang.eric80@gmail.com>
2023-02-21 15:57:32 -0500

    Added form and upload request

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await axios.post("http://localhost:5000/upload", {
+      url: e.target.url.value,
+    });
+    console.log(e.target.url.value);
+  };
   return (
     <div className="App">
-      <h1>VANISHLINKS LETS GO</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          URL: <input type="text" name="url"></input>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -2,19 +2,19 @@ const express = require("express");
 const axios = require("axios");
 
 const cors = require("cors");
-var bodyParser = require('body-parser'); // Parse JSON data
+var bodyParser = require("body-parser"); // Parse JSON data
 const fs = require("fs");
-var sqlite3 = require('sqlite3').verbose(); // Mongodb?
-var path = require("path")
+var sqlite3 = require("sqlite3").verbose(); // Mongodb?
+var path = require("path");
 //require("dotenv").config();
 
 const app = express();
-const BASE_URL = 'App base URL goes here';
+const BASE_URL = "App base URL goes here";
 
-var db = new sqlite3.Database('links.db');
-db.run('CREATE TABLE IF NOT EXISTS links(id TEXT, url TEXT)'); 
+var db = new sqlite3.Database("links.db");
+db.run("CREATE TABLE IF NOT EXISTS links(id TEXT, url TEXT)");
 
-app.engine('html', require('ejs').renderFile)
+app.engine("html", require("ejs").renderFile);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,47 +22,66 @@ app.use(bodyParser.json());
 
 app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get("/", function(req, res) {
-    res.sendFile(process.cwd() + "/public/index.html"); // index.html will be the landing page
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/public/index.html"); // index.html will be the landing page
 });
 
-function randomstring(len) { // Generate a random string of len characters
-    var res = '';
-    var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (var i = 0; i < len; i++) {
-        res += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return res;
+function randomstring(len) {
+  // Generate a random string of len characters
+  var res = "";
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < len; i++) {
+    res += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return res;
 }
 
-app.post("/api/uploaded", function(req, res) {
-    var url = req.body.url.toString();
-	var new_url = randomstring(5);
-    db.serialize(() => {
-        db.run('INSERT INTO links(id,url) VALUES(?,?)', [new_url, url], function(err) {
-            if (err) {
-                return console.log(err.message);
-            }
-            console.log(new_url, req.body.url);
-            res.render(process.cwd() + "/public/success.html", { new_url: new_url, base_url: BASE_URL }); // success.html will display the results of our post
-        });
-    });
+// Eric
+app.post("/upload", (req, res) => {
+  res.send(req.body.url.toString());
+  console.log(req.body.url.toString());
+});
+// End Eric
+
+app.post("/api/uploaded", function (req, res) {
+  var url = req.body.url.toString();
+  var new_url = randomstring(5);
+  db.serialize(() => {
+    db.run(
+      "INSERT INTO links(id,url) VALUES(?,?)",
+      [new_url, url],
+      function (err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        console.log(new_url, req.body.url);
+        res.render(process.cwd() + "/public/success.html", {
+          new_url: new_url,
+          base_url: BASE_URL,
+        }); // success.html will display the results of our post
+      }
+    );
+  });
 });
 
 app.get("/links/:link", (req, res) => {
-	var key = req.params.link.toString();
-    console.log(key);
-    db.serialize(() => {
-        db.each('SELECT id ID, url URL FROM links WHERE id =?', [key], function(err,row) {
-            if (err) {
-                res.render(process.cwd() + "/public/failure.html"); // failure.html will display an error message
-                console.log(err.message);
-            }
-            console.log("URL: ", row.URL); // delete after redirect
-            res.redirect(`${row.URL}`);
-            // TODO: delete the entry from our databse
-        });
-    });
+  var key = req.params.link.toString();
+  console.log(key);
+  db.serialize(() => {
+    db.each(
+      "SELECT id ID, url URL FROM links WHERE id =?",
+      [key],
+      function (err, row) {
+        if (err) {
+          res.render(process.cwd() + "/public/failure.html"); // failure.html will display an error message
+          console.log(err.message);
+        }
+        console.log("URL: ", row.URL); // delete after redirect
+        res.redirect(`${row.URL}`);
+        // TODO: delete the entry from our databse
+      }
+    );
+  });
 });
 
 app.listen(5000, () => {
